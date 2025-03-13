c8d4da643316f756fdca1f8fb09019b4b1081ad6
ruffsin <ruffsin@github.com>
2025-03-13 10:41:58 +0100

    Lade till test1.py och uppdaterade kodstrukturen

---
+++ test.py
@@ -0,0 +1,8 @@
+namn = input("Vad heter du? ")
+fodelsear = int(input("Vilket år föddes du? "))
+
+# Beräkna när användaren kommer att fylla 100 år
+ar_fyller_100 = fodelsear + 100
+
+# Skriv ut resultatet
+print(f"Hej {namn}, du kommer att fylla 100 år, år {ar_fyller_100}.")
